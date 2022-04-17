a6b27c3fc98d802038c64b01ba4c551fafa772a7
khoitran-hoodi <khoitran.hoodi@gmail.com>
2022-04-17 13:00:50 +0700

    datatypes of js

---
+++ dataTypes.js
@@ -0,0 +1,12 @@
+let a = "hehe";
+let b = 10;
+let arr = [a, b];
+let hehe = "hihi";
+console.log(arr[1]);
+let obj = {
+  name: "hoodi",
+  age: 19,
+};
+console.log(obj.age + arr[1]);
+console.log(1 === "1");
+console.log(1 == "1");

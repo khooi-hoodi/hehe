15b365968c9f443919cf20f6aa0160161965b6de
khoitran-hoodi <khoitran.hoodi@gmail.com>
2022-04-17 13:19:48 +0700

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,10 @@
+const age = 1;
+if (age > 21) {
+  if (age <= 80) {
+    console.log("You are young");
+  } else {
+    console.log("You are Old");
+  }
+} else {
+  console.log("You are Illegal");
+}

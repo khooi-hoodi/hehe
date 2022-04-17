836748b1fb41f80bd872e2ed7645f2ff830cda2a
khoitran-hoodi <khoitran.hoodi@gmail.com>
2022-04-17 13:41:35 +0700

    loops

---
+++ loops.js
@@ -0,0 +1,13 @@
+let weight = 80;
+const year = 5;
+for (let i = 0; i < year; i++) {
+  weight = weight - 1;
+}
+
+let isGay = false;
+while (isGay) {
+  console.log("hehe");
+}
+do {
+  console.log("heheDoWhile");
+} while (isGay);
